f2856f2af3af8b5200213398b584b9923bdaeb69
S.Vasilev <s.vasilev9169@gmail.com>
2018-09-09 22:51:24 +0300

    changing paths

--- layout_manager.php
+++ layout_manager.php
@@ -1,7 +1,7 @@
 <?php     
         
   function loginform() {
-        echo "<form id='index-form' action='./forum/validatelogin.php' method='POST'>
+        echo "<form id='index-form' action='./validatelogin.php' method='POST'>
               <h1 id = 'form-sing'>Sing Up</h1>
               <p class='usr'>Username:</p>
               <input type='text' name='username' id='username' placeholder='Username...'' />
@@ -19,6 +19,6 @@
 function logout()
     {
         echo ("<p class='greeting'>Wellcome ".$_SESSION['username']."!\nEnjoy!</p>
-        <form action='./forum/logout.php' method ='GET' type='button' class='btn btn-primary' data-toggle='modal' data-target='.bd-example-modal-lg'>
+        <form action='./logout.php' method ='GET' type='button' class='btn btn-primary' data-toggle='modal' data-target='.bd-example-modal-lg'>
         <input id='log-out-submit' type='submit' value='Logout' /></form>");
     }
