8bd3e3b715d2d03c595b82553a00773da2529cfb
S.Vasilev <s.vasilev9169@gmail.com>
2018-09-09 21:52:14 +0300

    changing paths

--- layout_manager.php
+++ layout_manager.php
@@ -1,7 +1,7 @@
 <?php     
         
   function loginform() {
-        echo "<form id='index-form' action='/forum/validatelogin.php' method='POST'>
+        echo "<form id='index-form' action='./forum/validatelogin.php' method='POST'>
               <h1 id = 'form-sing'>Sing Up</h1>
               <p class='usr'>Username:</p>
               <input type='text' name='username' id='username' placeholder='Username...'' />
@@ -10,7 +10,7 @@
               <input type='password' name='password' id='password' placeholder='Password...''/>
               
               <input type='submit' value='Login' />
-              <button type='button' onclick='location.href=\"/forum/registration.html\";'>Register</button>
+              <button type='button' onclick='location.href=\"./forum/registration.html\";'>Register</button>
             </form>";
           }
 ?>
@@ -19,6 +19,6 @@
 function logout()
     {
         echo ("<p class='greeting'>Wellcome ".$_SESSION['username']."!\nEnjoy!</p>
-        <form action='/forum/logout.php' method ='GET' type='button' class='btn btn-primary' data-toggle='modal' data-target='.bd-example-modal-lg'>
+        <form action='./forum/logout.php' method ='GET' type='button' class='btn btn-primary' data-toggle='modal' data-target='.bd-example-modal-lg'>
         <input id='log-out-submit' type='submit' value='Logout' /></form>");
     }
