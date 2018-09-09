6c44af7de30327cab9e9fba0b60987788591c78e
S.Vasilev <s.vasilev9169@gmail.com>
2018-09-09 21:24:39 +0300

    changing connection file

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
 
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db_name = 'forum';
+    $host = 'den1.mysql1.gear.host';
+    $user = 'forumdb01';
+    $pass = 'Cr1IcE__04Wv';
+    $db_name = 'forumdb01';
     $connection = mysqli_connect($host, $user, $pass, $db_name);
