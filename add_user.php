081f968320c910f32971455193660af7aefb14f1
S.Vasilev <s.vasilev9169@gmail.com>
2018-09-11 21:24:00 +0300

    adding checks for empty user and pass

--- add_user.php
+++ add_user.php
@@ -1,12 +1,19 @@
 <?php
 
+    $msg = "";
     include ('db_connection.php');
 
     $usr = $_POST['username'];
     $psw = $_POST['password'];
 
-    $insert = mysqli_query($connection, "insert into t_user (`user`, `pass`)
-                                            values('".$usr."','".$psw."');");
+    
+    if ($usr == "" || $psw == ""){
+        $msg = "Emty User or Password!";
+    }
+    else{
+        $insert = mysqli_query($connection, "insert into t_user (`user`, `pass`)
+                                             values('".$usr."','".$psw."');");
+    }
 
     if ($insert){
         header("Location: ./index.php?status=reg_success");
