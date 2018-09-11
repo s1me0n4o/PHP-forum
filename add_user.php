0568e435cf191b7041789c53422dd6cd1efc88bc
S.Vasilev <s.vasilev9169@gmail.com>
2018-09-11 22:04:53 +0300

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,24 +1,16 @@
 <?php
 
-    $msg = "";
     include ('db_connection.php');
 
     $usr = $_POST['username'];
     $psw = $_POST['password'];
 
     
-    if ($usr == "" || $psw == ""){
-        $msg = "Emty User or Password!";
-    }
-    else{
+  
         $insert = mysqli_query($connection, "insert into t_user (`user`, `pass`)
                                              values('".$usr."','".$psw."');");
-    }
+   
 
     if ($insert){
         header("Location: ./index.php?status=reg_success");
-    }
-    else {
-        header("Location: ./registration.html")
-        echo "Emty User or Password!"
     }
